263126083e020bdea03beddedc32af7a25618db2
Berger <berger@cs.rwth-aachen.de>
2021-04-22 11:25:14 +0200

    LTE variant is maybe more like a phone?

--- lineage.mk
+++ lineage.mk
@@ -6,7 +6,7 @@ TARGET_SCREEN_WIDTH := 768
 TARGET_SCREEN_HEIGHT := 1024
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
